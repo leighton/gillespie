90b15c6d5fafbaa998bc7adcf70daeabd1cf01fe
Leighton Turner <leightonturner@gmail.com>
2015-09-03 06:43:30 +0100

    I'm not sure

--- pred_prey.py
+++ pred_prey.py
@@ -4,9 +4,10 @@ from sympy import symbols
 import numpy as np
 import matplotlib.pyplot as plt
 from stochastic import integrate
+from stochastic import cython_propensity_function
 
-X, Y, Z = symbols('X Y Z')
-B, l, mu, nu = symbols('β λ μ ν')
+X, Y, Z = symbols('X Y Z', integer=True)
+B, l, mu, nu = symbols('β λ μ ν', real=True)
 
 x = [X, Y, Z]
 T = [
@@ -31,16 +32,25 @@ parm = {
   B: 0.001,
   l: 2,
   mu: 0.05,
-  nu: 0.01,
+  nu: 0.01
 }
+#import profile
 
-print "Start simulation"
-y = integrate(x, T, prop, parm, ics, 500)
-print "Stop simulation"
+#print "Start simulation"
+def run():
+  y = list(integrate(x, T, prop, parm, ics, 500))
+  return y
 
-t, x1, x2, x3 = np.transpose(y);
+x = run()
+t, x1, x2, x3 = np.transpose(x);
 plt.plot(t, x1, t, x2, t, x3)
 plt.show()
+
+x = run()
+
+
+#profile.run("run(); print")
+
 
 
 
--- stochastic.py
+++ stochastic.py
@@ -1,16 +1,32 @@
 import numpy as np
-from random import random
+from numpy.random import random
 from sympy.utilities.lambdify import lambdify, lambdastr
 from sympy.utilities.codegen import codegen
 import sympy.mpmath as mp
+import cython
+
+from sympy.utilities.autowrap import ufuncify
+from sympy.printing.theanocode import theano_function
 
 #def create_c_propensity_function(x, parm, prop):
 #  [(c_name, c_code), (h_name, c_header)] = codegen(("propensity", np.array(prop)), "C", "test", header=False, empty=False)
 #  print c_code
 
-def create_propensity_function(x, parm, prop):
+"""
+TODO: look into:
+ - https://github.com/Theano/
+ - http://docs.scipy.org/doc/scipy/reference/generated/scipy.weave.inline.html
+ - cython.inline
+"""
+
+def py_propensity_function(x, parm, prop):
   args = x+parm.keys()
-  return (lambdify(args, prop), args)
+  return (lambdify(args, prop, 'numpy'), args)
+
+def cython_propensity_function(x, parm, prop):
+  args = x+parm.keys()
+  return (theano_function(args, prop), args)
+  #return (cython.inline("return "+lambdastr(args, prop, dummify=True)), args)
 
 """
  s: symbolic states of the model
@@ -19,63 +35,59 @@ def create_propensity_function(x, parm, prop):
  ics: intitial conditions
  t: simulation duration
 """
-def integrate(x, T, prop, parm, ics, tmax, method="direct"):
+def integrate(x, T, prop, parm, ics, tmax, method="direct", propensity_function=py_propensity_function):
 
-  if len(T) != len(prop):
+  trans_n = len(T)
+
+  if trans_n != len(prop):
     raise Error("Transition and propensity vectors must be of same size")
 
   #time!
   t = 0
 
-#  create_c_propensity_function(x, parm, prop)
-
   #convert sparse state space to dense state space
   xv = dict([(xi, ics.get(xi, 0)) for xi in x])
 
-  #create initial state vector with zeroth realisation
-  #TODO: replace with sparse vector
-  y = [ [t] + [xv[xi] for xi in x] ]
+  #yield zeroth realisation
+  #TODO: replace with sparse vector?
+  yield [t] + [xv[xi] for xi in x]
 
   model_map = xv.copy()
   model_map.update(parm)
 
-  prop_fn, prop_args = create_propensity_function(x, parm, prop)
+  prop_fn, prop_args = propensity_function(x, parm, prop)
 
   while t < tmax:
 
     #evaluate propensity vector
-    #propv = np.array(map(lambda p: p.evalf(subs=model_map), prop))
+    #propv = np.asarray(map(lambda p: p.evalf(subs=model_map), prop))
 
     args = [model_map[p] for p in prop_args]
-    propv = np.array(prop_fn(*args))
+    prop_v = np.asarray(prop_fn(*args))
 
     #calculate total propensity
-    propt = np.sum(propv)
+    prop_t = np.sum(prop_v)
 
     #no more propensity, system is at rest
-    if propt == 0:
-      raise Exception("No more propensity? "+ str(propv))
-      return y
+    if prop_t == 0:
+      #TODO: should return a single last point of zeros(len(x)) at time tmax 
+      raise Exception("No more propensity? "+ str(prop_v))
 
-    #determine transition probability
     #TODO: allow for negative values? e.g. abs(propv)/np.sum(abs(propv))
-    prob = propv/propt
-
-    r = random()
+    #determine transition probability
+    prob = prop_v/prop_t
 
     #determine future event time
-    tau = (1/propt)*np.log(1/r)
+    tau = (1/prop_t)*np.log(1/random())
+    t+=tau
 
-    rxn = np.random.choice(len(prop), 1, p=list(prob))[0]
+    rxn = np.random.choice(trans_n, 1, p=list(prob))[0]
 
     for state, delta in T[rxn].items():
       xv[state] = xv[state] + delta
       model_map[state] = model_map[state] + delta
 
-    t+=tau
+    yield [t]+[xv[xi] for xi in x]
 
-    yi = [t]+[xv[xi] for xi in x]
-    y.append(yi)
-
-  return y
+  raise StopIteration()
 
